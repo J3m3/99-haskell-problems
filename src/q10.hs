49348867f0bedadc6de48fa9f804d0af0ff2db2c
YJS <y.j3ms.n@gmail.com>
2023-12-06 14:16:53 +0000

    feat: add implementation using Data.List.group

--- src/q10.hs
+++ src/q10.hs
@@ -1,3 +1,5 @@
+import Data.List qualified (group)
+
 encode :: (Eq a) => [a] -> [(Int, a)]
 encode xs = reverse $ go (0, head xs) [] xs
   where
@@ -6,3 +8,6 @@ encode xs = reverse $ go (0, head xs) [] xs
     go acc whole (x : xs)
       | snd acc == x = go (fst acc + 1, x) whole xs
       | otherwise = go (1, x) (acc : whole) xs
+
+encode' :: (Eq a) => [a] -> [(Int, a)]
+encode' xs = [(length x, head x) | x <- Data.List.group xs]
